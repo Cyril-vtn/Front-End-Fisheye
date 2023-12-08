b3665a295cfdff4732c5d0b12b4342dbeede5810
Cyril-vtn <votion.cyril@gmail.com>
2023-12-08 17:09:29 +0100

    change template function to display data and return card for each photographer & comments add

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,38 @@
+// This function is used to create a template for a photographer.
+// It takes in a data object as a parameter which contains information about the photographer.
+
 function photographerTemplate(data) {
-    const { name, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    // Destructuring the data object to get the necessary fields.
+    const { name, portrait, id, city, country, price, tagline } = data;
 
+    // Constructing the path to the photographer's portrait.
+    const picture = `assets/Photographers ID Photos/${portrait}`;
+
+    // This function is used to create a DOM element for the photographer's card.
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+        const photographCard = document.createElement('article');
+        photographCard.classList.add('ph__card');
+
+        const photographer = `
+            <a  href="photograph.html?id=${id}" aria-label="Profil de ${name}">
+                <div class="ph__container__img">
+                    <img src="${picture}" alt=${name}/>
+                </div>
+                <h1 class="ph__name">${name}</h1>
+            </a>
+            <div class="ph__location">
+                <h3>${city}, ${country}</h3>
+            </div>
+            <p class="ph__tagline">${tagline}</p>
+            <p class="ph__price">${price}€ /jour</p>
+        `;
+
+        // Setting the innerHTML of the created element to the photographer's card HTML.
+        photographCard.innerHTML = photographer;
+        // Returning the created element.
+        return photographCard;
     }
-    return { name, picture, getUserCardDOM }
+    // Returning an object with the getUserCardDOM function.
+    return { getUserCardDOM }
 }
